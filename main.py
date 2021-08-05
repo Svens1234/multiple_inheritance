e8d1770d8d41eb321d415575cfd965078ba6fc9d
Svenden123 <buhrus1@yandex.ru>
2021-08-05 12:18:12 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+class Swimmable:
+    def __init__(self, name):
+        print('Method init() of Swimmable')
+        self.name = name
+
+    def greeting(self):
+        print(f'Hello! My name is {self.name} and I can swim')
+
+    def swim(self):
+        print('I\m swimming')
+
+
+class Walkable:
+    def __init__(self, name):
+        print('Method init() of Walkable')
+        self.name = name
+
+    def greeting(self):
+        print(f'Hello! My name is {self.name} and I can walk')
+
+    def walk(self):
+        print('I\m walking')
+
+
+
+
+class Flyable:
+    def __init__(self, name):
+        print('Method init() of Flyable')
+        self.name = name
+
+    def greeting(self):
+        print(f'Hello! My name is {self.name} and I can fly')
+
+    def fly(self):
+        print('I\m flying')
+
+
+class Gamecharacter(Swimmable, Walkable, Flyable):
+    def __init__(self, name):
+        print('Method init() of GameCharacter')
+        self.name = name
+        Swimmable.__init__(self, name)
+        Walkable.__init__(self, name)
+        Flyable.__init__(self, name)
+
+
+james = Gamecharacter('James')
+james.greeting()
+#james.swim()
+#james.walk()
+#james.fly()
+
+
+#print(isinstance(james, Swimmable))
+#print(isinstance(james, Walkable))
+#print(isinstance(james, Flyable))
+#print(isinstance(james, Gamecharacter))
